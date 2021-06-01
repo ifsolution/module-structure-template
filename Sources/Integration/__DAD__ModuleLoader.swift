590985835dfdaf5af81dd5ec43416964821a057a
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-06-01 11:06:38 +0700

    Moudle Loader Fix id

--- Sources/Integration/__DAD__ModuleLoader.swift
+++ Sources/Integration/__DAD__ModuleLoader.swift
@@ -17,7 +17,7 @@ struct __DAD__ModuleLoader: ModuleLoader {
         container.registerBoard(identifier) { id in
             let producer = __DAD__BoardProducer()
             let mainboard = Motherboard(identifier: "<#domain.root.main#>", boardProducer: producer)
-            return RootBoard(identifier: identifier, continuousBoard: mainboard)
+            return RootBoard(identifier: id, continuousBoard: mainboard)
         }
     }
 }
