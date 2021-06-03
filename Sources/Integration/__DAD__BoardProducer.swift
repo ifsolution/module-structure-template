1d39fd2ccf4e2911084dbe526d82b1e2212e2d99
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-06-03 15:24:40 +0700

    Add IDExtenstions

--- Sources/Integration/__DAD__BoardProducer.swift
+++ Sources/Integration/__DAD__BoardProducer.swift
@@ -10,6 +10,9 @@ import Boardy
 import Foundation
 
 struct __DAD__BoardProducer: ActivableBoardProducer {
+    let externalProducer: ActivableBoardProducer
+    let identifierExtensions: __DAD__IDExtensions
+
     func produceBoard(identifier: BoardID) -> ActivatableBoard? {
         switch identifier {
         default:
--- Sources/Integration/__DAD__ModulePlugin.swift
+++ Sources/Integration/__DAD__ModulePlugin.swift
@@ -10,18 +10,25 @@ import Boardy
 import DadFoundation
 import Foundation
 
+public struct __DAD__IDExtensions {
+    public init() {}
+}
+
 public struct __DAD__ModulePlugin: ModulePlugin {
     public let identifier: BoardID
+    public let identifierExtensions: __DAD__IDExtensions
 
-    public init(identifier: BoardID) {
+    public init(identifier: BoardID, identifierExtensions: __DAD__IDExtensions = __DAD__IDExtensions()) {
         self.identifier = identifier
+        self.identifierExtensions = identifierExtensions
     }
 
     public func apply(for main: MainComponent) {
-        main.registerBoard(identifier) { id in
-            let producer = __DAD__BoardProducer()
+        let mainProducer = main.producer
+        mainProducer.registerBoard(identifier) { identifier in
+            let producer = __DAD__BoardProducer(externalProducer: mainProducer, identifierExtensions: identifierExtensions)
             let mainboard = Motherboard(identifier: "<#domain.root.main#>", boardProducer: producer)
-            return RootBoard(identifier: id, continuousBoard: mainboard)
+            return RootBoard(identifier: identifier, continuousBoard: mainboard)
         }
     }
 }
