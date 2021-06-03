7e426a4ce5733ec3b9fbc2e299f3432f42c28276
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-06-03 22:51:55 +0700

    Add useful extensions

--- Sources/Shared/Extensions/Extensions.swift
+++ Sources/Shared/Extensions/Extensions.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import UIKit
+
+private class BundleToken {}
+
+extension Bundle {
+    static var current: Bundle { Bundle(for: BundleToken.self) }
+}
+
+extension UIImage {
+    convenience init?(xcnamed: String) {
+        self.init(named: xcnamed, in: .current, compatibleWith: nil)
+    }
+
+    static func from(xcnamed: String) -> UIImage? {
+        UIImage(xcnamed: xcnamed)
+    }
+}
