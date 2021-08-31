5492d8165572b5a63c82767e006ece8e5f37b0fc
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-08-31 20:02:34 +0700

    Update Plugin template

--- Sources/Integration/___VARIABLE_moduleName___ModulePlugin.swift
+++ Sources/Integration/___VARIABLE_moduleName___ModulePlugin.swift
@@ -3,56 +3,56 @@
 //  ___VARIABLE_moduleName___
 //
 //  Created by NGUYEN CHI CONG on 6/1/21.
-//  
+//
 //
 
+import __DAD__IO
 import Boardy
 import Foundation
-import __DAD__IO
 
 public struct ___VARIABLE_moduleName___ModulePlugin: ModulePlugin {
-    public let identifier: BoardID
-    // public let serviceType: ___VARIABLE_moduleName___ModulePlugin.ServiceType
-    // public let identifierExtensions: ___VARIABLE_moduleName___ModulePlugin.IDExtensions
+    public let service: ___VARIABLE_moduleName___ModulePlugin.ServiceType
 
-    public init(identifier: BoardID = ___VARIABLE_moduleName___Namespace.default) {
-                // identifierExtensions: ___VARIABLE_moduleName___ModulePlugin.IDExtensions = .init()) {
-        self.identifier = identifier
-        // self.serviceType = serviceType
-        // self.identifierExtensions = identifierExtensions
+    public init(service: ___VARIABLE_moduleName___ModulePlugin.ServiceType) {
+        self.service = service
     }
 
     public func apply(for main: MainComponent) {
         let mainProducer = main.producer
-        // let externalIDs = identifierExtensions
-        
-        mainProducer.registerBoard(identifier) { [unowned mainProducer] identifier in
-            return RootBoard(identifier: identifier, producer: BoardProducer(externalProducer: mainProducer, registrationsBuilder: { producer in
-                // <#registration code#>
-            }))
+
+        switch service {
+        case .default:
+            mainProducer.registerBoard(identifier) { [unowned mainProducer] identifier in
+                RootBoard(
+                    identifier: identifier,
+                    producer: BoardProducer(
+                        externalProducer: mainProducer,
+                        registrationsBuilder: { producer in
+                            // <#registration code#>
+                        }
+                    )
+                )
+            }
         }
     }
 
-    /// Use IDExtensions when you would like to match internal BoardID with external BoardID from external producer.
-    /// This make Internal Producer to be able produce external-defined Board with internal identifier.
-    // public struct IDExtensions {
-    //     public init() {}
-    // }
+    public var identifier: BoardID {
+        switch service {
+        case let .default(identifier):
+            return identifier
+        }
+    }
 
-    // public var identifier: BoardID {
-    //     switch serviceType {
-    //     case let .some(identifier):
-    //         return identifier
-    //     }
-    // }
-
-    // public enum ServiceType {
-    //     case some(BoardID)
-    // }
+    /// Each service is equivalent to one entry point
+    public enum ServiceType {
+        case `default`(BoardID)
+    }
 }
 
 extension ___VARIABLE_moduleName___ModulePlugin {
     public static var bundledPlugins: [ModulePlugin] {
-        [___VARIABLE_moduleName___ModulePlugin()]
+        return [
+            ___VARIABLE_moduleName___ModulePlugin(service: .default(___VARIABLE_moduleName___Namespace.default)),
+        ]
     }
 }
