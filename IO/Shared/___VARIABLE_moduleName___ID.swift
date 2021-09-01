6288e3d92a1a08d2a304e51ac0f32c3bbb92784a
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-09-01 22:31:00 +0700

    Rename Namespace to ID

--- IO/Shared/___VARIABLE_moduleName___ID.swift
+++ IO/Shared/___VARIABLE_moduleName___ID.swift
@@ -1,11 +1,11 @@
 //
-//  ___VARIABLE_moduleName___Namespace.swift
+//  ___VARIABLE_moduleName___ID.swift
 //  ___VARIABLE_moduleName___
 //
 //  Created by NGUYEN CHI CONG on 6/1/21.
 //  
 //
 
-public enum ___VARIABLE_moduleName___Namespace {
+public enum ___VARIABLE_moduleName___ID {
     public static let `default`: BoardID = .pub___VARIABLE_moduleName___
 }
