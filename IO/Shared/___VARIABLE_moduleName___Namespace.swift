c2351156312f832d7666f8f1066977c0ecc4daf9
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-09-01 07:19:07 +0700

    Avoid expose multiple namespaces of one module

---
+++ IO/Shared/___VARIABLE_moduleName___Namespace.swift
@@ -0,0 +1,11 @@
+//
+//  ___VARIABLE_moduleName___Namespace.swift
+//  ___VARIABLE_moduleName___
+//
+//  Created by NGUYEN CHI CONG on 6/1/21.
+//  
+//
+
+public enum ___VARIABLE_moduleName___Namespace {
+    public static let `default`: BoardID = .pub___VARIABLE_moduleName___
+}
