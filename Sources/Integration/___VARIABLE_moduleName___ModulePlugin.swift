5925f445ccd8bcb9181c1b5c4438f68d72d92573
NGUYEN CHI CONG <congnc.if@gmail.com>
2022-11-03 16:57:29 +0700

    Update integration plugin

--- Sources/Integration/___VARIABLE_moduleName___ModulePlugin.swift
+++ Sources/Integration/___VARIABLE_moduleName___ModulePlugin.swift
@@ -26,29 +26,29 @@ public struct ___VARIABLE_moduleName___ModulePlugin: ModulePlugin {
 
         switch service {
         case .default:
-            mainProducer.registerBoard(identifier) { identifier in
+            mainProducer.registerBoard(service.identifier) { identifier in
                 ___VARIABLE_moduleName___BoardFactory.make(identifier: identifier, producer: continuousProducer)
             }
         }
     }
 
-    public var identifier: BoardID {
-        switch service {
-        case let .default(identifier):
-            return identifier
-        }
-    }
-
     /// Each service is equivalent to one entry point
     public enum ServiceType {
-        case `default`(BoardID)
+        case `default`
+
+        public var identifier: BoardID {
+            switch self {
+                case .default:
+                    return .pub___VARIABLE_moduleName___
+            }
+        }
     }
 }
 
 extension ___VARIABLE_moduleName___ModulePlugin {
     public static var bundledPlugins: [ModulePlugin] {
         return [
-            ___VARIABLE_moduleName___ModulePlugin(service: .default(.pub___VARIABLE_moduleName___)),
+            ___VARIABLE_moduleName___ModulePlugin(service: .default),
         ]
     }
 }
