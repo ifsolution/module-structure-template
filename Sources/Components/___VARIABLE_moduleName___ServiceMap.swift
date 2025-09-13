62e618beba9900a26970deb722f12163c77c319f
NGUYEN CHI CONG <congnc.if@gmail.com>
2025-09-13 12:16:37 +0700

    Add internal service map

---
+++ Sources/Components/___VARIABLE_moduleName___ServiceMap.swift
@@ -0,0 +1,16 @@
+//
+//  ___VARIABLE_moduleName___ServiceMap.swift
+//  ___VARIABLE_moduleName___
+//
+//  Created by Boardy on 6/1/21.
+//
+//
+
+import Boardy
+import Foundation
+
+final class __DAD__ServiceMap: ServiceMap {}
+
+extension ServiceMap {
+    var ___VARIABLE_serviceMap___: __DAD__ServiceMap { link() }
+}
